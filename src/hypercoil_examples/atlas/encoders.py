08ab18379614d32fc83b376313ea8dc86e0cff60
Rastko Ciric <rastko@stanford.edu>
2024-01-28 18:50:28 -0800

    (CHK) mid-refactor checkpoint

--- src/hypercoil_examples/atlas/encoders.py
+++ src/hypercoil_examples/atlas/encoders.py
@@ -18,6 +18,12 @@ import equinox as eqx
 from hypercoil.engine import Tensor
 from hypercoil.functional import pairedcorr, compartmentalised_linear
 from hypercoil.functional.sphere import icosphere
+from hypercoil.init.atlas import (
+    BaseAtlas,
+    CortexSubcortexCIfTIAtlas,
+    IcosphereAtlas,
+)
+from hypercoil.nn.atlas import AtlasLinear
 from hyve import (
     Cell,
     plotdef,
@@ -27,6 +33,7 @@ from hyve import (
     vertex_to_face,
     plot_to_display,
     plot_to_image,
+    plot_to_html,
     save_figure,
     text_element,
 )
@@ -137,6 +144,23 @@ class IcosphereEncoder(eqx.Module):
         self.limits = limits
 
 
+class ConsensusAtlasEncoder(eqx.Module):
+    encoding: Mapping[str, Tensor]
+    limits: Mapping[str, Tuple[int, int]]
+
+    def __init__(
+        self,
+        consensus_atlas: BaseAtlas,
+        *,
+        key: Optional['jax.random.PRNGKey'] = None,
+    ):
+        self.encoding = {k: v.T for k, v in consensus_atlas.maps.items()}
+        self.limits = {
+            c: (o.slice_index, o.slice_size)
+            for c, o in consensus_atlas.compartments.items()
+        }
+
+
 def get_coords_and_mask_fslr(hemi: Literal['L', 'R'] = 'L') -> Tensor:
     mask = nb.load(
         tflow.get(
@@ -154,10 +178,10 @@ def get_coords_and_mask_fslr(hemi: Literal['L', 'R'] = 'L') -> Tensor:
     return ref, mask
 
 
-def main():
+def configure_icosphere_encoder() -> IcosphereEncoder:
     coords_L, mask_L = get_coords_and_mask_fslr(hemi='L')
     coords_R, mask_R = get_coords_and_mask_fslr(hemi='R')
-    encoder = IcosphereEncoder(
+    return IcosphereEncoder(
         subdivisions=SUBDIVISIONS,
         scale=0.5,
         point_coords={'L': coords_L, 'R': coords_R},
@@ -166,6 +190,31 @@ def main():
         rotation_secondary=np.array((0., 1., 0.)),
     )
 
+
+def configure_consensus_atlas_encoder() -> ConsensusAtlasEncoder:
+    atlas = CortexSubcortexCIfTIAtlas(
+        ref_pointer=(
+            '/Users/rastkociric/Downloads/combined_clusters/'
+            'abcd_template_matching_combined_clusters_thresh0.605.dlabel.nii'
+        ),
+        mask_L=None,
+        mask_R=None,
+        surf_L=tflow.get(
+            'fsLR', density='32k', hemi='L', space=None, suffix='sphere'
+        ),
+        surf_R=tflow.get(
+            'fsLR', density='32k', hemi='R', space=None, suffix='sphere'
+        ),
+    )
+    return ConsensusAtlasEncoder(atlas)
+
+
+def visualise_surface_encoder(
+    encoder_name: str,
+    array_L: Tensor,
+    array_R: Tensor,
+    is_masked: bool = True,
+):
     layout = Cell() / Cell() / Cell() << (1 / 3)
     layout = layout | layout | layout << (1 / 3)
     annotations = {
@@ -196,16 +245,16 @@ def main():
     plot_f = plotdef(
         surf_from_archive(),
         surf_scalars_from_array(
-            'icosphere',
-            is_masked=False,
+            'encoder',
+            is_masked=is_masked,
             allow_multihemisphere=False,
         ),
-        parcellate_colormap('icosphere', 'network', template='fsLR'),
-        #vertex_to_face('icosphere', interpolation='mode'),
+        parcellate_colormap('encoder', 'network', template='fsLR'),
+        #vertex_to_face('encoder', interpolation='mode'),
         #plot_to_display(),
         text_element(
             name='title',
-            content='Icosphere Encoder',
+            content=f'{encoder_name} Encoder',
             bounding_box_height=128,
             font_size_multiplier=0.2,
             font_color='#cccccc',
@@ -217,17 +266,15 @@ def main():
             padding=10,
             canvas_size=(900, 750),
             canvas_color=(0, 0, 0),
-            fname_spec='scalars-{surfscalars}',
+            fname_spec=f'scalars-{encoder_name}',
             scalar_bar_action='collect',
         ),
     )
-    disc_L = (encoder.encoding['L'].argmax(-1) + 1) * encoder.encoding['L'].max(-1)
-    disc_R = (encoder.encoding['R'].argmax(-1) + 1) * encoder.encoding['R'].max(-1)
     plot_f(
         template='fsLR',
         surf_projection='veryinflated',
-        icosphere_array_left=disc_L,
-        icosphere_array_right=disc_R,
+        encoder_array_left=array_L,
+        encoder_array_right=array_R,
         window_size=(600, 500),
         hemisphere=['left', 'right', 'both'],
         views={
@@ -239,6 +286,109 @@ def main():
         output_dir='/tmp',
         load_mask=True,
         # hemisphere='left',
+    )
+
+
+def main():
+    atlas = IcosphereAtlas(
+        name='Icosphere',
+        n_subdivisions=5,
+        rotation_target=np.array((0., 0., 1.)),
+        rotation_secondary=np.array((0., 1., 0.)),
+        disc_scale=0.5,
+    )
+    # map_L = atlas.maps['cortex_L']
+    # map_R = atlas.maps['cortex_R']
+    # visualise_surface_encoder(
+    #     encoder_name='Icosphere',
+    #     array_L=(map_L.argmax(0) + 1) * (map_L.max(0) > 0),
+    #     array_R=(map_R.argmax(0) + 1) * (map_R.max(0) > 0),
+    #     is_masked=True,
+    # )
+
+    model = AtlasLinear.from_atlas(atlas, encode=True, key=jax.random.PRNGKey(0))
+
+    # data_L = nb.load(
+    #     '/Users/rastkociric/Downloads/ds000224-fmriprep/sub-MSC01/ses-func01/'
+    #     'func/sub-MSC01_ses-func01_task-rest_hemi-L_space-fsaverage5_bold.func.gii'
+    # ).darrays[0].data
+    # data_R = nb.load(
+    #     '/Users/rastkociric/Downloads/ds000224-fmriprep/sub-MSC01/ses-func01/'
+    #     'func/sub-MSC01_ses-func01_task-rest_hemi-R_space-fsaverage5_bold.func.gii'
+    # ).darrays[0].data
+    # data = np.concatenate((data_L, data_R), axis=-1)
+    data_full = nb.load(
+        '/Users/rastkociric/Downloads/ds000224-fmriprep/sub-MSC01/ses-func02/func/'
+        'sub-MSC01_ses-func02_task-rest_space-fsLR_den-91k_bold.dtseries.nii'
+    ).get_fdata(dtype=np.float32).T
+    data = data_full[:atlas.mask.shape[0]][atlas.mask.mask_array]
+    enc = model(data)
+
+    junk_mask_L = np.isnan(enc['cortex_L'].sum(-1))
+    junk_mask_R = np.isnan(enc['cortex_R'].sum(-1))
+    plot_f = plotdef(
+        surf_from_archive(),
+        surf_scalars_from_array(
+            'encoder',
+            is_masked=True,
+        ),
+        #vertex_to_face('encoder', interpolation='mode'),
+        plot_to_html(),
+    )
+    plot_f(
+        template='fsLR',
+        surf_projection='veryinflated',
+        encoder_array_left=~junk_mask_L,
+        encoder_array_right=~junk_mask_R,
+        window_size=(600, 500),
+        surf_scalars_cmap='bone',
+        fname_spec=f'scalars-zerovariance',
+        output_dir='/tmp',
+        # theme=pv.themes.DarkTheme(),
+        # hemisphere='left',
+    )
+    # plot_f = plotdef(
+    #     surf_from_archive(),
+    #     surf_scalars_from_array(
+    #         'encoder',
+    #         is_masked=False,
+    #     ),
+    #     #vertex_to_face('encoder', interpolation='mode'),
+    #     plot_to_display(),
+    # )
+    # plot_f(
+    #     template='fsLR',
+    #     surf_projection='veryinflated',
+    #     # encoder_array_left=~junk_mask_L,
+    #     # encoder_array_right=~junk_mask_R,
+    #     encoder_array=~np.isclose(data_full[:atlas.mask.shape[0]].sum(-1), 0),
+    #     window_size=(600, 500),
+    #     surf_scalars_cmap='bone',
+    #     # theme=pv.themes.DarkTheme(),
+    #     # hemisphere='left',
+    # )
+    assert 0
+
+    # icosencoder = configure_icosphere_encoder()
+    # disc_L = (
+    #     icosencoder.encoding['L'].argmax(-1) + 1
+    # ) * icosencoder.encoding['L'].max(-1)
+    # disc_R = (
+    #     icosencoder.encoding['R'].argmax(-1) + 1
+    # ) * icosencoder.encoding['R'].max(-1)
+    # visualise_surface_encoder(
+    #     encoder_name='Icosphere',
+    #     array_L=disc_L,
+    #     array_R=disc_R,
+    #     is_masked=False,
+    # )
+    atlasencoder = configure_consensus_atlas_encoder()
+    atlas_L = atlasencoder.encoding['cortex_L'].argmax(-1)
+    atlas_R = atlasencoder.encoding['cortex_R'].argmax(-1)
+    visualise_surface_encoder(
+        encoder_name='Consensus Atlas',
+        array_L=atlas_L,
+        array_R=atlas_R,
     )
 
 
