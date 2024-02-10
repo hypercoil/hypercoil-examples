71def970217c78fa83e061c4f58968f65e8ea8aa
Rastko Ciric <rastko@stanford.edu>
2024-02-10 02:19:28 -0800

    (CHK) checkpoint mean template init for alignment

---
+++ src/hypercoil_examples/atlas/meaninit.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Mean atlas initialisation
+~~~~~~~~~~~~~~~~~~~~~~~~~
+Initialise an alignment atlas tensor as the mean of the input image encodings.
+"""
+import glob
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import nibabel as nb
+import numpy as np
+from hypercoil.functional import residualise
+from hypercoil.nn import AtlasLinear
+from hypercoil_examples.atlas.encoders import (
+    create_icosphere_encoder,
+)
+from hypercoil_examples.atlas.vmf import ENCODE_SELF, whiten_data
+
+IMGS = sorted(glob.glob(
+    '/Users/rastkociric/Downloads/ds000224-fmriprep/sub-MSC0*/ses-func0*/func/'
+    'sub-MSC0*_ses-func0*_task-rest_space-fsLR_den-91k_bold.dtseries.nii'
+))
+
+
+def main():
+    atlas = create_icosphere_encoder()
+    encoder = AtlasLinear.from_atlas(
+        atlas,
+        encode=True,
+        key=jax.random.PRNGKey(0),
+    )
+    for i, img in enumerate(IMGS):
+        print(f'Processing {img} ({i+1}/{len(IMGS)})')
+        data = nb.load(img)
+        data_full = jnp.asarray(data.get_fdata()).T
+        data = data_full[~data.header.get_axis(1).volume_mask] #[atlas.mask.mask_array]
+
+        gs = data.mean(0, keepdims=True)
+        data = residualise(data, gs)
+        # Plug zero-variance vertices with ramp (for no NaNs in log prob)
+        data = jnp.where(
+            (data.sum(-1) == 0)[..., None],
+            jnp.arange(data.shape[-1])[None, :],
+            data,
+        )
+        _, parcels_enc, _ = ENCODE_SELF['icosphere'](
+            model=encoder, data=data, atlas=atlas
+        )
+        enc = encoder(data)
+        enc = jnp.concatenate((enc['cortex_L'], enc['cortex_R']))
+        enc, parcels_enc = whiten_data(enc, parcels_enc)
+        enc = enc / jnp.linalg.norm(enc, axis=-1, keepdims=True)
+        if i == 0:
+            init = enc
+        else:
+            init += enc
+
+    init = init / len(IMGS)
+    plt.figure(figsize=(10, 10))
+    plt.hist(jnp.linalg.norm(init, axis=-1), bins=200)
+    plt.savefig('/tmp/mean_init.png')
+    np.save('/tmp/mean_init.npy', np.asarray(init), allow_pickle=False)
+    assert 0
+
+
+if __name__ == '__main__':
+    main()
