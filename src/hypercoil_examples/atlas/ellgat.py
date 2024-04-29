(FIX) parcellation model gradient NaN

src/hypercoil_examples/atlas/ellgat.py
@@ -89,8 +89,11 @@ class ELLGAT(eqx.Module):
         X = self.nlin(
             jnp.einsum('...hon,...honk->...honk', Q, K[..., adj])
         )
-        attn = jax.nn.softmax(jnp.where(adj == -1, -jnp.inf, X), axis=-1)
-        attn = jnp.where(jnp.isnan(attn), 0, attn)
+        X = jnp.where(adj == -1, -jnp.inf, X)
+        # The following line is required to avoid NaNs in the edge case
+        X = jnp.where((adj != -1).sum(-1, keepdims=True) == 0, 0, X)
+        attn = jax.nn.softmax(X, axis=-1)
+        # attn = jnp.where(jnp.isnan(attn), 0, attn)
         # attn = jnp.einsum(
         #     '...hwnk,...hw->...hnk',
         #     attn,
@@ -119,9 +122,11 @@ class UnitSphereNorm(eqx.Module):
         *,
         key: Optional['jax.random.PRNGKey'] = None,
     ) -> Tensor:
+        mask = jnp.all(X != 0, axis=-2, keepdims=True)
+        X = jnp.where(mask, X, 1)
         norm = jnp.linalg.norm(X, axis=-2, keepdims=True)
         norm = jnp.where(norm == 0, 1, norm)
-        return X / norm
+        return jnp.where(mask, X / norm, 0)
 
 
 class ELLGATBlock(eqx.Module):

src/hypercoil_examples/atlas/model.py
@@ -504,6 +504,7 @@ def forward(
     compartment: str,
     key: 'jax.random.PRNGKey',
 ):
+    key_m, key_n = jax.random.split(key, 2)
     result = model(
         coor=coor,
         encoder=encoder,
@@ -526,9 +527,17 @@ def forward(
     other_coor = _to_jax_array(
         model.regulariser[other_hemi].spatial_distribution.mu
     )
+    #TODO: We can get NaN if the two are aligned exactly. Here we add noise
+    #      to avoid this edge case, but we should work out why this occurs.
+    other_coor = other_coor + 1e-4 * jax.random.normal(
+        key_n, other_coor.shape
+    )
+    other_coor = other_coor / jnp.linalg.norm(
+        other_coor, axis=-1, keepdims=True
+    )
     tether = spherical_geodesic(
         _to_jax_array(
-            model.regulariser['cortex_L'].spatial_distribution.mu
+            model.regulariser[compartment].spatial_distribution.mu
         )[..., None, :],
         other_coor.at[..., 0].set(-other_coor[..., 0])[..., None, :],
     ).sum()