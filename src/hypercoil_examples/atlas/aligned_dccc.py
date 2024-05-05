(FIX) model fixes; distribution loss with better gradient

src/hypercoil_examples/atlas/aligned_dccc.py
@@ -120,10 +120,11 @@ def doublet_energy(
 
 def param_bimodal_beta(n_classes: int):
     """Bimodal beta distribution with a minimum at the maximum entropy"""
+    from hypercoil_examples.atlas.beta import BetaCompat
     alpha = jnp.log(n_classes) / (
         jnp.log(n_classes) - jnp.log(1 - 1 / n_classes)
-    )
-    return Beta(alpha, 1 - alpha)
+    ).item()
+    return BetaCompat(alpha, 1 - alpha)
 
 
 def compute_dccc(

src/hypercoil_examples/atlas/model.py
@@ -15,6 +15,7 @@ import jax
 import jax.numpy as jnp
 import equinox as eqx
 import numpyro
+from jax.scipy.special import logit
 from numpyro.distributions import Distribution
 from scipy.special import softmax
 
@@ -471,6 +472,7 @@ def init_full_model(
     import numpy as np
     from hypercoil_examples.atlas.spatialinit import init_spatial_priors
     key = key or jax.random.PRNGKey(0)
+    key_r, key_a = jax.random.split(key)
     temporal_encoder = configure_multiencoder()
     spatial_encoder = configure_geometric_encoder()
     spatial_loc_left, spatial_loc_right, spatial_data = init_spatial_priors()
@@ -502,7 +504,7 @@ def init_full_model(
         M=template,
     )
     coor_parcels_L = coor_L[jax.random.choice(
-        key,
+        key_r,
         coor_L.shape[0],
         shape=(num_parcels,),
         replace=False,
@@ -546,7 +548,7 @@ def init_full_model(
         )
         for compartment in ('cortex_L', 'cortex_R')
     }
-    doublet_distribution = param_bimodal_beta(jnp.sqrt(num_parcels))
+    doublet_distribution = param_bimodal_beta(jnp.sqrt(num_parcels).item())
     regulariser = {
         compartment: SpatialSelectiveMRF(
             spatial_distribution=spatial_distribution[compartment],
@@ -581,7 +583,7 @@ def init_full_model(
         norm=UnitSphereNorm(),
         dropout=ELLGAT_DROPOUT,
         dropout_inference=False,
-        key=jax.random.PRNGKey(0),
+        key=key_a,
     )
     model = ForwardParcellationModel(
         regulariser=regulariser,
@@ -601,7 +603,8 @@ def forward(
     energy_nu: float = 1.,
     recon_nu: float = 1.,
     tether_nu: float = 1.,
-    nkl_nu: float = 1e2,
+    div_nu: float = 1e3,
+    inference: bool = False,
     key: 'jax.random.PRNGKey',
 ):
     meta = {}
@@ -615,6 +618,7 @@ def forward(
         encoder=encoder,
         encoder_result=encoder_result,
         compartments=(compartment,),
+        inference=inference,
         key=key_m,
     )
     (_, (_, _, _, _, T)) = encoder_result
@@ -657,14 +661,21 @@ def forward(
         meta = {**meta, 'hemisphere_tether': tether}
     else:
         tether = 0
-    if nkl_nu != 0:
-        nkl = nkl_nu * (
-            jnp.log(P[compartment]) / P[compartment].shape[0]
-        ).sum(0).mean()
-        meta = {**meta, 'nkl': nkl}
+    if div_nu != 0:
+        maxent = 1 / P[compartment].shape[0]
+        div = div_nu * (
+            jnp.exp(-(
+                logit(
+                    P[compartment]
+                    # convex combine, else this term can grow without bound
+                    #(1 - maxent ** 2) * P[compartment] + maxent ** 3
+                ) - logit(maxent)
+            ) ** 2)
+        ).mean()
+        meta = {**meta, 'div': div}
     else:
-        nkl = 0
-    return energy + recon_error + tether + nkl, meta
+        div = 0
+    return energy + recon_error + tether + div, meta
 
 
 def main(subject: str = '01', session: str = '01', num_parcels: int = 100):

src/hypercoil_examples/atlas/train.py
@@ -42,6 +42,8 @@ from hyve import (
 )
 
 LEARNING_RATE = 0.001
+MAX_EPOCH = 10000
+SEED = 0
 REPORT_INTERVAL = 10
 CHECKPOINT_INTERVAL = 100
 PATHWAYS = ('regulariser', 'full') # ('full',) ('regulariser',)
@@ -50,10 +52,10 @@ SUBJECTS = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10',)
 SESSIONS = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10',)
 VISUALISE_MRF = True
 VISUALISE_SINGLE = True
-ENERGY_NU = 1.,
-RECON_NU = 1.,
-TETHER_NU = 1.,
-NKL_NU = 1e2,
+ENERGY_NU = 1.
+RECON_NU = 1.
+TETHER_NU = 1.
+DIV_NU = 1e3
 
 
 #jax.config.update('jax_debug_nans', True)
@@ -184,8 +186,8 @@ def update(
         energy_nu=ENERGY_NU,
         recon_nu=RECON_NU,
         tether_nu=TETHER_NU,
-        nkl_nu=NKL_NU,
-        key=jax.random.PRNGKey(0),
+        div_nu=DIV_NU,
+        key=key,
     )
     if jnp.isnan(loss):
         print(f'NaN loss at epoch {epoch}. Skipping update')
@@ -201,8 +203,11 @@ def update(
     return model, opt_state, loss.item(), {k: v.item() for k, v in meta.items()}
 
 
-def main(num_parcels: int = 100):
-    key = jax.random.PRNGKey(0)
+def main(
+    num_parcels: int = 100,
+    start_epoch: Optional[int] = 3400,
+):
+    key = jax.random.PRNGKey(SEED)
     data_entities = tuple(product(SESSIONS, SUBJECTS))
     num_entities = len(data_entities)
     coor_L, coor_R = get_coors()
@@ -223,7 +228,18 @@ def main(num_parcels: int = 100):
         'cortex_L': coor_L,
         'cortex_R': coor_R,
     }
-    for i in range(2000):
+    if start_epoch is not None:
+        model = eqx.tree_deserialise_leaves(
+            f'/tmp/parcellation_model_checkpoint{start_epoch}',
+            like=model,
+        )
+        opt_state = eqx.tree_deserialise_leaves(
+            f'/tmp/parcellation_optim_checkpoint{start_epoch}',
+            like=opt_state,
+        )
+    else:
+        start_epoch = -1
+    for i in range(start_epoch + 1, MAX_EPOCH):
         session, subject = data_entities[i % num_entities]
         print(f'Epoch {i} (sub-{subject} ses-{session})')
         try:
@@ -255,8 +271,8 @@ def main(num_parcels: int = 100):
                 'Skipping'
             )
             continue
-        key = jax.random.fold_in(key, i)
-        key_l, key_r = jax.random.split(key)
+        key_e = jax.random.fold_in(key, i)
+        key_l, key_r = jax.random.split(key_e)
         meta_L = {}
         meta_R = {}
         loss_ = 0
@@ -338,7 +354,8 @@ def main(num_parcels: int = 100):
                     encoder=encoder,
                     encoder_result=encoder_result,
                     compartments=('cortex_L', 'cortex_R'),
-                    key=jax.random.PRNGKey(0),
+                    inference=True,
+                    key=key,
                 )
                 visualise(
                     name=f'SingleSubj_epoch_{i}',
@@ -347,7 +364,7 @@ def main(num_parcels: int = 100):
                     plot_f=plot_f,
                 )
             if i % CHECKPOINT_INTERVAL == 0:
-                print('Serialising model parameters for checkpoint')
+                print('Serialising model and optimiser state for checkpoint')
                 eqx.tree_serialise_leaves(
                     f'/tmp/parcellation_model_checkpoint{i}',
                     model,