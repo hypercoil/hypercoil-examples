cf85a68ebcc7e106b4f43d2de990f84ced327b12
Rastko Ciric <rastko@stanford.edu>
2024-09-12 18:37:04 -0700

    (FIX) non-terrible denoise

--- src/hypercoil_examples/atlas/data.py
+++ src/hypercoil_examples/atlas/data.py
@@ -86,19 +86,33 @@ PARAM9KEY = sum(
 ) + ['white_matter', 'csf', 'global_signal']
 
 
+def _normalise(data: jnp.ndarray) -> jnp.ndarray:
+    data = data - data.mean(-1, keepdims=True)
+    data = data / data.std(-1, keepdims=True)
+    data = jnp.where(jnp.isnan(data), 0, data)
+    return data
+
+
 def _get_data(
     cifti: Optional[str] = None,
     confounds: Optional[str] = None,
     normalise: bool = True,
-    mgtr: bool = True,
-    gsr: bool = False,
+    denoising: Literal[
+        'mgtr',
+        'mgtr_deriv',
+        'gsr',
+        'gsr_deriv',
+        'param18',
+        'param36',
+        'mgtr+18',
+        'mgtr+36',
+    ] = 'mgtr',
     filter_rps: bool = True,
+    filter_data: bool = True,
     censor_thresh: float = 0.15,
     pad_to_size: Optional[int] = None,
     censor_method: Literal['drop', 'zero'] = 'drop',
     t_rep: float = 0.72,
-    filter_data: bool = True,
-    param36: bool = False,
     *,
     data: Optional[jnp.ndarray] = None,
     key: Optional['jax.random.PRNGKey'] = None,
@@ -116,35 +130,29 @@ def _get_data(
     if t_rep > 100: # Convert to seconds
         t_rep /= 1000
     if normalise:
-        data = data - data.mean(-1, keepdims=True)
-        data = data / data.std(-1, keepdims=True)
-        data = jnp.where(jnp.isnan(data), 0, data)
+        data = _normalise(data)
 
-    if mgtr or (gsr and not confounds):
+    confmodel = None
+    if denoising in ('mgtr', 'mgtr_deriv', 'mgtr+18', 'mgtr+36') or (
+        denoising in ('gsr', 'gsr_deriv') and not confounds
+    ):
         # This is really MGTR, not GSR
-        gs = data.mean(0, keepdims=True)
-        gsd = jnp.concatenate((np.zeros((1, 1)), np.diff(gs)), -1)
-        gs = jnp.concatenate((gs, gsd))
-        data = residualise(data, gs)
+        confmodel = data.mean(0, keepdims=True)
+        if denoising in ('mgtr_deriv', 'gsr_deriv', 'mgtr+18', 'mgtr+36'):
+            gsd = jnp.concatenate((np.zeros((1, 1)), np.diff(confmodel)), -1)
+            confmodel = jnp.concatenate((confmodel, gsd))
+            if denoising in ('mgtr+18', 'mgtr+36'):
+                confmodel = jnp.concatenate(
+                    (confmodel, confmodel ** 2), -1
+                )
+                mgtr = pd.DataFrame({
+                    'global_signal': confmodel[0],
+                    'global_signal_derivative1': confmodel[1],
+                    'global_signal_power2': confmodel[2],
+                    'global_signal_derivative1_power2': confmodel[3],
+                })
     if confounds:
         confounds = pd.read_csv(confounds, sep='\t')
-        if param36:
-            param18key = PARAM9KEY + [f'{e}_derivative1' for e in PARAM9KEY]
-            param36key = param18key + [f'{e}_power2' for e in param18key]
-            param36model = confounds[param36key].values.T
-            # So that nothing explodes when we invert
-            breakpoint()
-            param36model = param36model - param36model.mean(-1, keepdims=True)
-            param36model = param36model / param36model.std(-1, keepdims=True)
-            param36model = jnp.where(jnp.isnan(param36model), 0, param36model)
-            data = residualise(data, param36model)
-        elif gsr:
-            if mgtr:
-                raise ValueError('Combining GSR and MGTR')
-            gs = confounds['global_signal'].values[None]
-            gsd = jnp.concatenate((np.zeros((1, 1)), np.diff(gs)), -1)
-            gs = jnp.concatenate((gs, gsd))
-            data = residualise(data, gs)
         rp_cols = ['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z']
         rp_cols = [f'{e}_derivative1' for e in rp_cols]
         rps = np.array(confounds[rp_cols].values)
@@ -154,16 +162,52 @@ def _get_data(
             fc = 0.1 # Cut-off frequency of the filter (6 bpm)
             filt = butter(N=1, Wn=fc, btype='low', fs=fs)
             rps = filtfilt(*filt, rps, axis=0)
+            confounds[rp_cols] = rps
         fd = jnp.abs(rps).sum(-1)
         tmask = fd <= censor_thresh
-        surviving_frames = jnp.where(tmask)[0]
+        if denoising in ('mgtr+18', 'mgtr+36'):
+            confounds[mgtr.columns] = mgtr
+            if denoising == 'mgtr+18': denoising = 'param18'
+            if denoising == 'mgtr+36': denoising = 'param36'
+        if denoising in ('param18', 'param36'):
+            model_key = PARAM9KEY + [f'{e}_derivative1' for e in PARAM9KEY]
+            if denoising == 'param36':
+                model_key = model_key + [f'{e}_power2' for e in model_key]
+            confmodel = confounds[model_key].values.T
+            # So that nothing explodes when we invert
+            confmodel = _normalise(confmodel)
+        elif denoising in ('gsr', 'gsr_deriv'):
+            confmodel = confounds['global_signal'].values[None]
+            if denoising == 'gsr_deriv':
+                gsd = jnp.concatenate((np.zeros((1, 1)), np.diff(confmodel)), -1)
+                confmodel = jnp.concatenate((confmodel, gsd))
         if filter_data:
             fc = (0.01, 0.1)
             filt = butter(N=1, Wn=fc, btype='bandpass', fs=fs)
             if jnp.any(~tmask):
-                breakpoint()
-                jnp.interp
+                f_interp = jax.vmap(
+                    jnp.interp,
+                    in_axes=(None, None, 0),
+                    out_axes=0,
+                )
+                data_interp = f_interp(
+                    jnp.where(~tmask)[0],
+                    jnp.where(tmask)[0],
+                    data[..., tmask],
+                )
+                data = data.at[..., ~tmask].set(data_interp)
+                if confmodel is not None:
+                    confmodel = confmodel.at[..., ~tmask].set(
+                        f_interp(
+                            jnp.where(~tmask)[0],
+                            jnp.where(tmask)[0],
+                            confmodel[..., tmask],
+                        )
+                    )
             data = filtfilt(*filt, data, axis=-1)
+            if confmodel is not None:
+                confmodel = filtfilt(*filt, confmodel, axis=-1)
+        surviving_frames = jnp.where(tmask)[0]
         if pad_to_size is not None:
             base_size = tmask.sum()
             if pad_to_size <= base_size:
@@ -187,8 +231,51 @@ def _get_data(
             index = surviving_frames
         if censor_method == 'drop':
             data = data[..., index]
+            if confmodel is not None:
+                confmodel = confmodel[..., index]
         else:
             data = jnp.where(tmask[None, ...], data, 0)
+            if confmodel is not None:
+                confmodel = jnp.where(tmask[None, ...], confmodel, 0)
+    data = residualise(data, confmodel)
     # Plug zero-variance vertices with ramp (for no NaNs in log prob)
     data = inject_noise_to_zero_variance(data, key=key)
     return data
+
+
+def main():
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from hypercoil.functional import sym2vec
+    from hyve_examples import get_schaefer400_cifti
+    cifti, confounds = get_msc_dataset('01', '01', task='rest', get_confounds=True)
+    data = _get_data(cifti, confounds, denoising='mgtr+36', censor_thresh=0.1)
+
+    rsts = pd.read_csv(
+        (
+            '/Users/rastkociric/Downloads/miniMSC/data/ts/'
+            'sub-MSC01_ses-func01_task-rest_run-01_schaefer400_ts.1D'
+        ),
+        sep=' ',
+        header=None,
+    )
+    ref = np.corrcoef(rsts.T)
+    plt.imshow(ref, vmin=-0.25, vmax=0.25, cmap='RdYlBu_r'); plt.show()
+
+    atlas = nb.load(get_schaefer400_cifti()).get_fdata().astype(int) - 1
+    atlas_mat = np.where(
+        (atlas < 0)[..., None],
+        0,
+        np.eye(atlas.max() + 1)[atlas],
+    ).squeeze().T
+    data_parc = jnp.linalg.lstsq(atlas_mat.T, data)[0]
+    result = np.corrcoef(data_parc)
+    plt.imshow(result, vmin=-0.25, vmax=0.25, cmap='RdYlBu_r'); plt.show()
+    print(np.corrcoef(sym2vec(result), sym2vec(ref))[0, 1])
+    sns.kdeplot(data=pd.DataFrame({'ref': sym2vec(ref), 'result': sym2vec(result)}))
+    plt.show()
+    breakpoint()
+
+
+if __name__ == '__main__':
+    main()
